b71d4ecd310a52972e08f5fdfcf63ce7219a20f1
RedArr <miao8627@gmail.com>
2018-11-02 14:19:23 +0800

    first commit

---
+++ application/index/controller/User.php
@@ -0,0 +1,20 @@
+<?php
+namespace app\index\controller;
+
+use think\Controller;
+
+class User extends Controller
+{
+    public function index()
+    {
+       return $this->fetch();
+    }
+    public function register()
+    {
+        return $this->fetch();
+    }
+    public function login()
+    {
+        return $this->fetch();
+    }
+}
